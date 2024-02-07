87bc0e4fe4b4a5dc70ff227b4a22d6550f5589c0
Mohamed Faiz M <faizmd614@gmail.com>
2024-02-07 23:28:40 +0530

    cntdown commit

--- symbo/effex/script.js
+++ symbo/effex/script.js
@@ -18,4 +18,24 @@ document.addEventListener('DOMContentLoaded',()=>{
             iterat+=1/3;
         },50);
     }
+
+    let upcomingDate = new Date("mar 16, 2024 12:00:00").getTime();
+    setInterval(() => {
+        let curDate = new Date();
+        let diff = upcomingDate - curDate;
+
+        let dLeft = Math.floor(diff / (1000 * 60 * 60 * 24));
+        let hLeft = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let mLeft = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+        let sLeft = Math.floor((diff % (1000 * 60)) / 1000);
+
+        if (sLeft < 0) {
+            stopInterval();
+        }
+
+        document.getElementById('days').innerHTML = dLeft;
+        document.getElementById('hours').innerHTML = hLeft;
+        document.getElementById('minutes').innerHTML = mLeft;
+        document.getElementById('seconds').innerHTML = sLeft;
+    }, 1000);
 })
